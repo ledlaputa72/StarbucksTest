59632a80278e19331e9049077cbef97af588c07d
ledlaputa72 <ledlaputa@gmail.com>
2021-05-14 14:36:27 +0900

    up

--- js/main.js
+++ js/main.js
@@ -63,9 +63,9 @@ new Swiper(".promotion .swiper-container", {
     spaceBetween: 10, //슬라이드 사이 여백
     centeredSlides: true, //1번 슬라이드가 가운데 보이기
     loop: true,
-    /* autoplay: {
+    autoplay: {
         delay: 5000,
-    }, */
+    },
     pagination: {
         el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
         clickable: true,
